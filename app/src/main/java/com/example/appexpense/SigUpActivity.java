5d498d8f85128e6ece2aa132a0a0b0d0477db0ef
haanh99 <haanh82867@gmail.com>
2023-12-04 13:37:06 +0700

    SigUpActivity

---
+++ app/src/main/java/com/example/appexpense/SigUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.appexpense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sig_up);
+    }
+}
