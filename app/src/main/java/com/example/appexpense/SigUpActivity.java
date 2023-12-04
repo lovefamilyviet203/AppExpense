0de84cc9318e1af56bbcc1c10b543d1b68f2465e
haanh99 <haanh82867@gmail.com>
2023-12-04 15:42:16 +0700

    commit commnet

--- app/src/main/java/com/example/appexpense/SigUpActivity.java
+++ app/src/main/java/com/example/appexpense/SigUpActivity.java
@@ -10,5 +10,7 @@ public class SigUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sig_up);
+        //This i commment
+        //Comement
     }
 }
